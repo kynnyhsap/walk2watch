ca86bb9b5fd732304ca0e0587a1243e285253c37
Andriy Pashynnyk <tobirawork@gmail.com>
2023-05-21 12:55:40 +0300

    fix

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,21 +1,22 @@
 exports.handler = async function (event, context) {
-  console.log(event, context)
   const body = JSON.parse(event.body)
-  console.log(body)
-  // const startDates = body.steps.startDates.split("\n")
-  // const endDates = body.steps.endDates.split("\n")
-  // const values = body.steps.values.split("\n")
-  //
-  // const steps = values.map((value, i) => {
-  //   return {
-  //     value,
-  //     startDate: startDates[i],
-  //     endDate: endDates[i],
-  //   }
-  // })
-  // console.log(steps);
+
+  const startDates = body.steps.startDates.split("\n")
+  const endDates = body.steps.endDates.split("\n")
+  const values = body.steps.values.split("\n")
+
+  const steps = values.map((value, i) => {
+    return {
+      value,
+      startDate: startDates[i],
+      endDate: endDates[i],
+    }
+  })
+
+  console.log(steps);
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ steps }),
   };
 };
