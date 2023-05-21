653c136ff8b09617cd018df3ec7af4bc5b346e68
Andriy Pashynnyk <tobirawork@gmail.com>
2023-05-21 12:43:55 +0300

    parse shortcut input

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,6 +1,19 @@
 exports.handler = async function (event, context) {
   console.log(event, context)
+  const body = JSON.parse(event.body)
 
+  const startDates = body.steps.startDates.split("\n")
+  const endDates = body.steps.endDates.split("\n")
+  const values = body.steps.values.split("\n")
+
+  const steps = values.map((value, i) => {
+    return {
+      value,
+      startDate: startDates[i],
+      endDate: endDates[i],
+    }
+  })
+  console.log(steps);
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World" }),
