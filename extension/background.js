f4bdd12ecd138296cd597514a6e292f287cf7284
Andriy Pashynnyk <tobirawork@gmail.com>
2023-05-28 17:37:53 +0300

    fix extension worker logic

--- extension/background.js
+++ extension/background.js
@@ -1,53 +1,68 @@
 const TIME_CHUNCK = 5000;
 const URI = "https://walk2watch.netlify.app";
 const WATCH_TIME_URI = `${URI}/.netlify/functions/watch-time`;
+const YOUTUBE_WATCH_URI = "https://www.youtube.com/watch";
 
-let timeoutId;
+let intervalId;
 
 function redirect() {
+  console.log("Redirecting to", URI);
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.update(tabs[0].id, { url: URI });
   });
 }
 
-async function sendPostRequest() {
-  await fetch(WATCH_TIME_URI, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      ms: TIME_CHUNCK,
-      datetime: new Date(),
-    }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.watchtimeLeft === 0) {
-        redirect();
-      } else {
-        timeoutId = setTimeout(sendPostRequest, TIME_CHUNCK);
-      }
-    })
-    .catch((err) => console.log(err));
+function stopWatching() {
+  console.log("Stop watching");
+
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
 }
 
-function sendGetRequest() {
-  fetch(WATCH_TIME_URI)
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.watchtimeLeft === 0) {
-        redirect();
-      } else {
-        sendPostRequest();
-      }
-    })
-    .catch(console.error);
-}
+async function startWatching() {
+  console.log("Start watching");
 
-// chrome.runtime.onInstalled.addListener(sendGetRequest);
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url.includes("youtube.com")) {
-    if (!timeoutId) {
-      sendGetRequest();
+  if (intervalId) return;
+
+  const { watchtimeLeft } = await fetch(WATCH_TIME_URI).then((res) =>
+    res.json()
+  );
+
+  console.log("Watchtime left", watchtimeLeft);
+
+  if (watchtimeLeft === 0) {
+    redirect();
+  }
+
+  intervalId = setInterval(async () => {
+    const { watchtimeLeft } = await fetch(WATCH_TIME_URI, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        ms: TIME_CHUNCK,
+        datetime: new Date(),
+      }),
+    }).then((res) => res.json());
+
+    console.log("Watchtime left", watchtimeLeft);
+
+    if (watchtimeLeft === 0) {
+      stopWatching(); 
+      redirect();
     }
+  }, TIME_CHUNCK);
+}
+
+const isYouTubeTab = (tab) => tab.url.includes(YOUTUBE_WATCH_URI);
+
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  const tab = await chrome.tabs.get(activeInfo.tabId);
+
+  if (isYouTubeTab(tab)) {
+    startWatching();
+  } else {
+    stopWatching();
   }
 });
